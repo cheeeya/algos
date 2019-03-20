ebbfafafcf36aec7686c1ab0d8fce2625e1afa3f
cheeeya <samueldchia@gmail.com>
2019-03-19 23:07:57 -0700

    array insertion methods

--- ruby01.rb
+++ ruby01.rb
@@ -104,4 +104,35 @@
         return arr.drop(n)
     end
 
+# 8. Ruby Array - Addition
 
+    # In this challenge, your task is to complete three functions that take in the array arr and
+        # Add an element to the end of the list
+        # Add an element to the beginning of the list
+        # Add an element after a given index (position)
+        # Add more than one element after a given index (position)
+
+        def end_arr_add(arr, element)
+            # Add `element` to the end of the Array variable `arr` and return `arr`
+            return arr.push(element)
+        end
+        
+        def begin_arr_add(arr, element)
+            # Add `element` to the beginning of the Array variable `arr` and return `arr`
+            return arr.unshift(element)
+        end
+        
+        def index_arr_add(arr, index, element)
+            # Add `element` at position `index` to the Array variable `arr` and return `arr`
+            return arr.insert(index, element)
+        end
+        
+        def index_arr_multiple_add(arr, index, *args)
+            # add any two elements to the arr at the index
+             return arr.insert(index, nil, nil) unless args.length > 0
+            args.each do |element|
+                arr.insert(index, element)
+                index += 1
+            end
+            return arr
+        end

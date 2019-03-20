81f03a0255f9090feef1d2716af1a9c6380c2510
cheeeya <samueldchia@gmail.com>
2019-03-19 22:48:54 -0700

    array indexing

--- ruby01.rb
+++ ruby01.rb
@@ -53,3 +53,58 @@
 
     array3 = [10,10]
     array_3 = Array.new(2,10)
+
+# 6. Array Indexing
+
+    def element_at(arr, index)
+        # return the element of the Array variable `arr` at the position `index`
+        # arr.at(index) # or
+        # arr[index]
+        return arr[index]
+    end
+
+    def inclusive_range(arr, start_pos, end_pos)
+        # return the elements of the Array variable `arr` between the start_pos and end_pos (both inclusive)
+        return arr[start_pos..end_pos]
+    end
+
+    def non_inclusive_range(arr, start_pos, end_pos)
+        # return the elements of the Array variable `arr`, start_pos inclusive and end_pos exclusive
+        return arr[start_pos...end_pos]
+    end
+
+    def start_and_length(arr, start_pos, length)
+        # return `length` elements of the Array variable `arr` starting from `start_pos`
+        return arr[start_pos, length]
+    end
+# 7. Array Indexing 2
+
+    def neg_pos(arr, index)
+        # return the element of the array at the position `index` from the end of the list
+        # Clue : arr[-index]
+        return arr[-index]
+    end
+
+    def first_element(arr)
+        # return the first element of the array
+        # arr.first
+        return arr.first
+    end
+
+    def last_element(arr)
+        # return the last element of the array
+        # arr.last
+        return arr.last
+    end
+
+    def first_n(arr, n)
+        # return the first n elements of the array
+        return arr.take(n)
+    end
+
+    def drop_n(arr, n)
+        # drop the first n elements of the array and return the rest
+        return arr.drop(n)
+    end
+
+

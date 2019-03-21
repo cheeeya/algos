619169ab2efb2f748bc4d9343c3426518fea838e
cheeeya <samueldchia@gmail.com>
2019-03-20 21:04:49 -0700

    array selection methods

--- ruby01.rb
+++ ruby01.rb
@@ -174,3 +174,25 @@
         # delete all the elements of the array where element = val
         arr.delete(val)
     end
+
+# 11. Array Selection
+
+    def select_arr(arr)
+        # select and return all odd numbers from the Array variable `arr`
+        arr.select { |v| v % 2 != 0 }
+    end
+    
+    def reject_arr(arr)
+        # reject all elements which are divisible by 3
+        arr.reject { |v| v % 3 == 0 }
+    end
+    
+    def delete_arr(arr)
+        # delete all negative elements
+        arr.delete_if { |v| v < 0 }
+    end
+    
+    def keep_arr(arr)
+        # keep all non negative elements ( >= 0)
+        arr.keep_if { |v| v >= 0 }
+    end

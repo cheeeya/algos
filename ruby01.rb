fe9e1b41c5fa8bcdba4964b5bf52b4dc61675a94
cheeeya <samueldchia@gmail.com>
2019-03-22 22:22:09 -0700

    enumerables intro

--- ruby01.rb
+++ ruby01.rb
@@ -260,3 +260,15 @@
         return str
     end
     
+# 16. Enumerables
+
+    # In this challenge, you have been provided with a custom object called colors that defines its own each method. You need to iterate over the items and return an Array containing the values.
+
+    def iterate_colors(colors)
+        # Your code here
+        colors_arr = Array.new
+        colors.each do |color|
+            colors_arr.push(color)
+        end
+        return colors_arr
+    end

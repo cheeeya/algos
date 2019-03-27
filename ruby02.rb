9720c2cad1f6b9948fc95f5f4af44e14133bd857
cheeeya <samueldchia@gmail.com>
2019-03-27 12:36:28 -0700

    transcode to UTF-8

--- ruby02.rb
+++ ruby02.rb
@@ -19,4 +19,11 @@
         HERE
     end
 
-# 2. 
+# 2. Encoding
+    # In this challenge, we practice setting the encoding information for some string of text using Ruby's Encoding methods.
+    # Write a function named transcode which takes a ISO-8859-1 encoded string as a parameter, converts it to an UTF-8 encoded string, and returns the result.
+
+    def transcode (string)
+        string.force_encoding(Encoding::UTF_8)
+    end
+

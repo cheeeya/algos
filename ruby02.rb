f0f7f60af7e35b3e4b3cd5cacf55d937af15daf8
cheeeya <samueldchia@gmail.com>
2019-03-27 12:23:09 -0700

    using strings with single quotes, double quotes, and here docs

--- ruby02.rb
+++ ruby02.rb
@@ -1,4 +1,22 @@
 # Ruby Strings
 
 # 1. Intro
+
+    def single_quote
+        # single quote string here
+        'Hello World and others!'
+    end
     
+    def double_quote
+        # Double quote string here
+        "Hello World and others!"
+    end
+    
+    def here_doc
+        # Here doc string here
+        <<-HERE
+            Hello World and others!
+        HERE
+    end
+
+# 2. 

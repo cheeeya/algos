a43346d33bb068ae638c7971d7470fa098448193
cheeeya <samueldchia@gmail.com>
2019-03-27 12:52:08 -0700

    use string indexing to truncate strings

--- ruby03.rb
+++ ruby03.rb
@@ -27,3 +27,13 @@
         string.force_encoding(Encoding::UTF_8)
     end
 
+# 3. Indexing
+    # In this challenge, your task is to code a serial_average method which is described below:
+        # It takes a fixed width string in format: SSS-XX.XX-YY.YY. SSS is a three digit serial number, XX.XX and YY.YY are two digit numbers including up to two decimal digits.
+        # It returns a string containing the answer in format SSS-ZZ.ZZ where SSS is the serial number of that input string, and ZZ.ZZ is the average of XX.XX and YY.YY.
+        # All numbers are rounded off to two decimal places.
+
+    def serial_average(serial_num)
+        average = (serial_num[4,5].to_f + serial_num[10,5].to_f) / 2.0
+        "#{serial_num[0,3]}-#{average.round(2)}"
+    end

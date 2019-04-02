7b3f040d177374608c1de4d422371604f2e3cfe8
cheeeya <samueldchia@gmail.com>
2019-04-01 17:16:26 -0700

    sum array elements

--- ruby04.rb
+++ ruby04.rb
@@ -9,6 +9,21 @@
     sum = solveMeFirst(val1,val2)
     print (sum)
 
-# 2.
+# 2. Simple Array Sum
   
-  
+    def simpleArraySum(ar)
+        ar.reduce(:+)
+    end
+
+    fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+    ar_count = gets.to_i
+
+    ar = gets.rstrip.split(' ').map(&:to_i)
+
+    result = simpleArraySum ar
+
+    fptr.write result
+    fptr.write "\n"
+
+    fptr.close()

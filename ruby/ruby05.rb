f8f56945f917c7d01c1a5bb6df751be360afff85
cheeeya <samueldchia@gmail.com>
2019-04-10 16:16:00 -0700

    [solved] Between Two Sets: find all numbers where all numbers in set A are a factor of, and is a factor of all numbers in set B

--- ruby/ruby05.rb
+++ ruby/ruby05.rb
@@ -62,3 +62,15 @@
             "NO"
         end
     end
+
+# 14. Between Two Sets
+    # You will be given two arrays of integers and asked to determine all integers that satisfy the following two conditions.
+        # The elements of the first array are all factors of the integer being considered
+        # The integer being considered is a factor of all elements of the second array
+    # Return the number of integers that are betwen the sets
+
+    def get_total_x(a,b)
+        (a.max..b.min).count{ |f| a.all?{ |x| f % x == 0 } && b.all?{ |y| y % f == 0 }}
+    end
+
+    

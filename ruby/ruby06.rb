b6fd53737c817a1672adc29c2e419bbba7c9deec
cheeeya <samueldchia@gmail.com>
2019-04-11 18:31:57 -0700

    [solved] Sock Merchant: determine the number of pairs of matching values in an array

--- ruby/ruby06.rb
+++ ruby/ruby06.rb
@@ -1,4 +1,12 @@
 # Problem Solving 21 - 30
 
 # 21. Sock Merchant
+    # Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+    # sock_merchant has the following parameter(s):
+        # n: the number of socks in the pile
+        # ar: the colors of each sock
 
+    def sock_merchant(n, ar)
+        sock_hash = ar.each_with_object(Hash.new(0)) { |sock, hash| hash[sock] += 1 }
+        sock_hash.values.map { |count| count / 2}.reduce(:+)
+    end

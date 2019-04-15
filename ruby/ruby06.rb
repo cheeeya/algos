8473cfcdc8d9759c01f4e8307938ad53836d5d00
cheeeya <samueldchia@gmail.com>
2019-04-14 23:18:48 -0700

    Page Count: Given the number of pages in a book and a page number to turn to, calculate the minimum number of pages to turn to reach that page.

--- ruby/ruby06.rb
+++ ruby/ruby06.rb
@@ -10,3 +10,14 @@
         sock_hash = ar.each_with_object(Hash.new(0)) { |sock, hash| hash[sock] += 1 }
         sock_hash.values.map { |count| count / 2}.reduce(:+)
     end
+
+# 22. Drawing Book
+    # When she opens the book, page 1 is always on the right side:
+    # return the minimum number of pages Brie must turn.
+    # pageCount has the following parameter(s):
+        # n: the number of pages in the book
+        # p: the page number to turn to
+
+    def page_count(n, p)
+        p / 2 < ((n + 1) % 2 + n - p) / 2 ? p / 2 : ((n + 1) % 2 + n - p) / 2
+    end

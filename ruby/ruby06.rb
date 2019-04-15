c47177b3de7e085b49024423bb751c43c8a90bb0
cheeeya <samueldchia@gmail.com>
2019-04-14 23:39:02 -0700

    [solved] Counting Valleys: given a string denoting uphill and downhill steps, calculate the number of valleys crossed

--- ruby/ruby06.rb
+++ ruby/ruby06.rb
@@ -14,10 +14,29 @@
 # 22. Drawing Book
     # When she opens the book, page 1 is always on the right side:
     # return the minimum number of pages Brie must turn.
-    # pageCount has the following parameter(s):
+    # page_count has the following parameter(s):
         # n: the number of pages in the book
         # p: the page number to turn to
 
     def page_count(n, p)
         p / 2 < ((n + 1) % 2 + n - p) / 2 ? p / 2 : ((n + 1) % 2 + n - p) / 2
     end
+
+# 23. Counting Valleys
+    # A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+    # A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+    # return an integer that denotes the number of valleys Gary traversed.
+    # counting_valleys has the following parameter(s):
+        # n: the number of steps Gary takes
+        # s: a string describing his path, D = downhill step, U = uphill step, each step a 1 unit change in altitude
+
+    def counting_valleys(n, s)
+        altitude, prev, count = 0, 0, 0
+        s.each_char do |char|
+            prev = altitude
+            char == 'D' ? altitude -= 1 : altitude += 1
+            count += 1 if altitude == 0 && prev < 0
+        end
+        count
+    end
+    

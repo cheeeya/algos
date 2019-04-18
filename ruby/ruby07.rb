fb9156d3e611506d02642644a37ec85561a58c2e
cheeeya <samueldchia@gmail.com>
2019-04-18 15:17:57 -0700

    [solved] Utopian Tree: implemented both dynamic and recursive solutions

--- ruby/ruby07.rb
+++ ruby/ruby07.rb
@@ -1,6 +1,24 @@
 # Problem Solving 31 - 40
 
 # 31. Utopian Tree
-    # Complete the utopianTree function in the editor below. It should return the integer height of the tree after the input number of growth cycles.
+    # The Utopian Tree sapling has a height of 1. 
+    # It goes through 2 cycles of growth every year.
+    # Each spring, it doubles in height. Each summer, its height increases by 1 meter
+    # If planted in the spring, return the integer height of the tree after the input number of growth cycles.
     # utopian_tree has the following parameter(s):
         # n: an integer, the number of growth cycles to simulate
+
+    # dynamic programming
+    def utopian_tree n
+        u = [1]
+        (1..n).each do |i|
+            i % 2 == 0 ? u[i] = u[i - 1] + 1 : u[i] = u[i - 1] * 2 
+        end
+        u[n]
+    end
+
+    # recursion
+    def utopian_tree n
+        return n + 1 if n < 3
+        n % 2 == 0 ? utopian_tree(n - 1) + 1 : utopian_tree(n - 1) * 2
+    end

b44dbe66cad0f81cefc0d06ee1a12ef1287ff717
cheeeya <samueldchia@gmail.com>
2019-05-02 19:06:48 -0700

    [started] Append and Delete

---
+++ ruby/ruby08.rb
@@ -0,0 +1,18 @@
+# Problem Solving 41-50
+
+# 41. Append and Delete
+    # You have a string of lowercase English alphabetic letters. You can perform two types of operations on the string:
+        # Append a lowercase English alphabetic letter to the end of the string.
+        # Delete the last character in the string. Performing this operation on an empty string results in an empty string.
+    # Given an integer, k, and two strings, s and t, determine whether or not you can convert s to t by performing exactly k of the above operations on s.
+    # If it's possible, print Yes. Otherwise, print No.
+    # return a string, either Yes or No.
+    # append_and_delete has the following parameter(s):
+        # s: the initial string
+        # t: the desired string
+        # k: an integer that represents the number of operations
+
+    def append_and_delete(s, t, k)
+        
+
+    end
